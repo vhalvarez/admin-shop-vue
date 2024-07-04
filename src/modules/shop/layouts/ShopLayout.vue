c13e8c855306c4afb21c41b8a81706da76d0619b
vhalvarez <alvarez3197@gmail.com>
2024-07-03 22:46:34 -0400

    ShopLayout - :art: de la tienda

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <!-- Header Navbar -->
+  <top-menu />
+
+
+
+  <!-- Router View -->
+  <router-view />
+
+  <!-- Footer -->
+  <custom-footer />
+</template>
+
+<script setup lang="ts">
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+</script>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import ShopLayout from '@/modules/shop/layouts/ShopLayout.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
+    {
+      path: '/',
+      name: 'shop',
+      component: ShopLayout,
+      children: [
+        { path: '', name: 'home', component: () => import('@/modules/shop/views/HomeView.vue') },
+      ],
+    },
     // {
     //   path: '/about',
     //   name: 'about',
@@ -17,7 +20,7 @@ const router = createRouter({
     //   // which is lazy-loaded when the route is visited.
     //   component: () => import('../views/AboutView.vue')
     // }
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
