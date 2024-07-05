bf1f191cfbc53128aeb675e319c524a1f998dfb2
vhalvarez <alvarez3197@gmail.com>
2024-07-05 18:53:47 -0400

    :pencil: Mostrar productos en pantalla

---
+++ src/modules/products/components/ProductList.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="py-10 bg-gray-100">
+    <div
+      class="mx-auto grid max-w-6xl grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+    >
+      <ProductCard v-for="product in products" :key="product.id" :product="product" />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import type { Product } from '../interfaces/products.interface';
+import ProductCard from './ProductCard.vue';
+
+interface Props {
+  products: Product[];
+}
+
+defineProps<Props>();
+</script>
